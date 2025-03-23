8db06a68510dc7c4d299e81d42d66bf7c6735320
windmaya <windmaya@gmail.com>
2025-03-23 12:33:27 +0800

    Add files via upload

---
+++ handle_kms.bat
@@ -0,0 +1,6 @@
+REM activate Office
+cd C:\Program Files\Microsoft Office\Office16
+cscript ospp.vbs /sethst:kms.jihuowin.com && cscript ospp.vbs /act
+
+REM activate Windows
+REM slmgr /skms kms.jihuowin.com && slmgr /ato
