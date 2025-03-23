e176ab75df657c2e50c2e0203c2458a9b44d825d
windmaya <windmaya@gmail.com>
2025-03-23 12:55:48 +0800

    Update handle_kms.bat

--- handle_kms.bat
+++ handle_kms.bat
@@ -2,5 +2,5 @@ REM activate Office
 cd C:\Program Files\Microsoft Office\Office16
 cscript ospp.vbs /sethst:kms.jihuowin.com && cscript ospp.vbs /act
 
-REM activate Windows
+REM activate Windows（Simplify 版已提供定期 KMS 啟動功能）
 REM slmgr /skms kms.jihuowin.com && slmgr /ato
